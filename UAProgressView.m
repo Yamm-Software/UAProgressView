8a821f1ec554a751095f35f4331db7cd12cccfbf
Elliot Sinyor <git@elliotsinyor.com>
2014-09-19 12:17:52 -0400

    change path layout method to draw counterclockwise.

--- UAProgressView.m
+++ UAProgressView.m
@@ -344,7 +344,8 @@ NSString * const UAProgressViewProgressAnimationKey = @"UAProgressViewProgressAn
 - (UIBezierPath *)layoutPath {
     const double TWO_M_PI = 2.0 * M_PI;
     const double startAngle = 0.75 * TWO_M_PI;
-    const double endAngle = startAngle + TWO_M_PI;
+    //const double endAngle = startAngle + TWO_M_PI; //clockwise, we add 2pi
+    const double endAngle = startAngle + TWO_M_PI; //counterclockwise, subtract 2pi
     
     CGFloat width = self.frame.size.width;
 	CGFloat borderWidth = self.shapeLayer.borderWidth;
@@ -352,7 +353,7 @@ NSString * const UAProgressViewProgressAnimationKey = @"UAProgressViewProgressAn
                                           radius:width/2.0f + (borderWidth / 2.0f) + 2.0 // - borderWidth
                                       startAngle:startAngle
                                         endAngle:endAngle
-                                       clockwise:YES];
+                                       clockwise:NO];
 }
 
 - (void)updateProgress:(float)progress {
